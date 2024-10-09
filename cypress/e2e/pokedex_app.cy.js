1449ed47089278b0b6540538cd030deee819e574
Riku <134082872+rigozu9@users.noreply.github.com>
2024-10-09 20:33:48 +0300

    11.9 done now correctly changed localhost address

--- cypress/e2e/pokedex_app.cy.js
+++ cypress/e2e/pokedex_app.cy.js
@@ -1,12 +1,12 @@
 /* global cy */
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:8080')
+    cy.visit('http://localhost:5000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
   it('navigate from the main page to the page of a particular Pokemon', function() {
-    cy.visit('http://localhost:8080/pokemon/charizard')
+    cy.visit('http://localhost:5000/pokemon/charizard')
     cy.contains('charizard')
     cy.contains('speed')
     cy.contains('solar power')
