6a82db2dfbd502346ac0b2349e32317e0aaa1b48
Riku <134082872+rigozu9@users.noreply.github.com>
2024-12-16 12:59:23 +0200

    heatl check error check

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  // change here
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
   res.send('ok')
 })
 
