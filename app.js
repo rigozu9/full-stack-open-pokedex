664ffe35017595bfb18d8cd4300c975f337188b7
Riku <134082872+rigozu9@users.noreply.github.com>
2025-01-14 10:40:55 +0200

    #skip broke the code to check if health check is working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-app.get('/version', (req, res) => {
+app.get('/version', (req, res) => 
   res.send('1') // change this string to ensure a new version deployed
 })
 
