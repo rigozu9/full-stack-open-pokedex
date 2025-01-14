cf0cbad2936b2b3771b6993f21973d328dfb3025
Riku <134082872+rigozu9@users.noreply.github.com>
2025-01-14 14:18:41 +0200

    fixed code and set 24hr schedule

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const mapResults = (({ results }) => results.map(({ url, name }) => ({
   id: parseInt(url.match(/\/(\d+)\//)[1])
 })))
 
-const App = () => 
+const App = () => {
   const match = useMatch('/pokemon/:name')
   const { data: pokemonList, error, isLoading } = useApi('https://pokeapi.co/api/v2/pokemon/?limit=50', mapResults)
 
